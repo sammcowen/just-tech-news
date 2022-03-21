9140319e63c389357fff02a9cdad44b590b189a9
HBK <sammcowen@gmail.com>
2022-03-21 00:16:10 -0700

    forgot to push home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,21 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req,res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post-url',
+            'title',
+        'created_at',
+[sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)', vote_count]
+       ],
+       include:[
+           {
+               model:'Comment',
+               attributes:['id', ]
+           }
+       ]
+    })
     res.render('homepage', {
         id:1,
         post_url: 'https;//handelbarsjs.com/guide/',
